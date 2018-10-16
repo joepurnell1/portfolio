516f9e43c8ac74966a3198404fa845d03b573498
Joe <joepurnell1@gmail.com>
2018-10-16 21:10:32 +0100

    definitely named that wrong...

--- src/components/atoms/layout/flexGridContainer/index.spec.js
+++ src/components/atoms/layout/flexGridContainer/index.spec.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import FlexContainer from './index';
+import FlexGridContainer from './index';
 
 describe('flex container component', () => {
   it('should render default as expected', () => {
     const render = renderer.create(
-      <FlexContainer>Will you stand above me</FlexContainer>,
+      <FlexGridContainer>Will you stand above me</FlexGridContainer>,
     );
     expect(render.toJSON()).toMatchSnapshot();
   });
 
   it('should render with given maxWidth as expected', () => {
     const render = renderer.create(
-      <FlexContainer maxWidth="500px">Look my way, never love me</FlexContainer>,
+      <FlexGridContainer maxWidth="500px">Look my way, never love me</FlexGridContainer>,
     );
     expect(render.toJSON()).toMatchSnapshot();
   });
