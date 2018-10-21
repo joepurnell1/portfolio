cf827257c56ab21a52d7aef16533ff4d132a3483
Joe <joepurnell1@gmail.com>
2018-10-21 17:37:21 +0100

    Swelling from the middle

--- src/components/molecules/swellingImageLink/index.js
+++ src/components/molecules/swellingImageLink/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import propTypes from './propTypes';
 
-const StyledLink = styled('a')``;
+const StyledLink = styled('a')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: ${p => p.size};
+  width: ${p => p.size};
+`;
 
 const StyledImage = styled('img')`
   height: ${p => p.height};
@@ -18,7 +24,10 @@ const StyledImage = styled('img')`
 const SwellingLink = ({
   link, imageSource, imageAlt, size, hoverSize,
 }) => (
-  <StyledLink href={link}>
+  <StyledLink
+    href={link}
+    size={hoverSize}
+  >
     <StyledImage
       src={imageSource}
       alt={imageAlt}
--- src/components/organisms/colouredInfoBand/index.js
+++ src/components/organisms/colouredInfoBand/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { withTheme } from 'styled-components';
+import styled, { withTheme } from 'styled-components';
 import propTypes, { defaultProps } from './propTypes';
 import ColouredContainer from '../../atoms/layout/colouredContainer';
 import HeaderText from '../../atoms/text/header';
 import TaglineText from '../../atoms/text/tagline';
+import SwellingImageLink from '../../molecules/swellingImageLink';
+import Github from '../../../images/social/github.svg';
+
+const LinkBand = styled('div')`
+  min-width: 300px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 50px;
+  height: 45px;
+`;
+
+const SwellingLink = styled(SwellingImageLink).attrs({
+  size: '40px',
+  hoverSize: '45px',
+})``;
 
 const ColouredInfoBand = ({ theme, title, subtitle }) => (
   <ColouredContainer backgroundColour={theme.ColouredInfoBand.backgroundColour}>
     <HeaderText colour={theme.ColouredInfoBand.textColour}>{title}</HeaderText>
     <TaglineText colour={theme.ColouredInfoBand.textColour}>{subtitle}</TaglineText>
+    <LinkBand>
+      <SwellingLink
+        imageSource={Github}
+        imageAlt="github"
+      />
+    </LinkBand>
   </ColouredContainer>
 );
 
