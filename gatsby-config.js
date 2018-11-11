a86c24ba63c1cd0fb567817ea2c5bb406c9df7be
Joe <joepurnell1@gmail.com>
2018-11-11 00:26:13 +0000

    adding sitemap generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.example.com',
+  },
   plugins: [
     {
       resolve: 'gatsby-plugin-styled-components',
     },
     'gatsby-plugin-eslint',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
   ],
 };
